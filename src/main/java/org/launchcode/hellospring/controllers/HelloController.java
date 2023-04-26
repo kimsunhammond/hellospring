28c150523465767b54bdb7ed9046a337ba9fd76b
kimsunhammond <kimsunhammond@gmail.com>
2023-04-26 17:28:47 -0500

    "thymeleaf chapter code-along"

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,53 +1,62 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@ResponseBody
-@RequestMapping("hello")
+
+//@RequestMapping("hello")
 public class HelloController {
 
     // Handles requests at path /hello
-//    @GetMapping("hello") // Specifies that it handles GET requests
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, Spring!";
-//    }
+    @GetMapping("hello") // Specifies that it handles GET requests
+    @ResponseBody
+    public String hello() {
+        return "Hello, Spring!";
+    }
 
     // Lives at /hello/goodbye
     @GetMapping("goodbye")
-    //@ResponseBody
+    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
     // Lives at /hello/hello
     // Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    //@ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     // Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    //@ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    @GetMapping("hello/{name}")
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        //String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        return "hello";
     }
 
     @GetMapping("form")
-    //@ResponseBody
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action='hello'>" + // submit a request to /hello
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
+    }
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 
 }
+
+
